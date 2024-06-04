a69086d1f1c4cf60aa512fa6ac0e8305faa29fd3
kortsa <leoklomegah8@gmail.com>
2024-06-04 14:01:09 +0000

    few touches

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,22 +1,9 @@
 
 import folder from "../../assets/icons8-bag-100.png";
-
+import { experiences } from "../../Constants/experience";
 import "./Experience.css";
 function Experience() {
   
-  const experiences = [
-    {
-      date: "November,2023 - Present",
-      title: "National Service @NodeEight",
-      desc: "After completion of University, i joined NodeEight for my National Service. During my National Service, i was part of the first Cohort of the jesi program and i joined the Software class. We learned Javascript and React.js for Frontend development and we also learned NodeJs for Backend",
-    },
-    {
-      date: "may,2022 - August, 2022",
-      title: "Intership @NodeEight",
-      desc: "NodEight is an innovative Hub at Ho where i did my school intership at. I worked with the media department for a month and half where i learned photography and graphic designing. i was later moved to the tech department where i learned web programming.",
-    },
-  
-  ];
   return (
     <div className="experience-content container">
       <div className="title">
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,66 +1,8 @@
 import folder from "../../assets/icons8-folder-32.png";
-import foodapp from "../../assets/foodapp.png";
-import sampleportfolio from "../../assets/sampleportfolio.png";
-import landingPage from "../../assets/landingpage.png"
-import votingapp from "../../assets/votingapp.png"
-import quote from "../../assets/quotegen.png"
-import clothingapp from "../../assets/clothingapp.png"
-import todo from "../../assets/todo.png"
+
 import "./Projects.css";
+import { projectLists } from "../../Constants/projectLists";
 function Projects() {
-  
-  const projectLists = [
-    {
-      image: foodapp,
-      title: "Food App",
-      description:
-        "This a simple food web app built which allow users to navigate through their favourite meals. A user can select number of meals, place and order navigate to the payment page. The app was built using React.Js and Vanilla CSS ",
-      link: "https://food-app-ruddy-delta.vercel.app/",
-    },
-    {
-      image: todo,
-      title: "Todo List",
-      description:
-        "It is a Todo list app which allows users to add, delete and edit daily tasks The app was built using React.Js and Vanilla CSS ",
-      link: "https://todo-list-r-js.vercel.app/k",
-    },
-     
-    {
-      image: clothingapp,
-      title: "Clothing App",
-      description:
-        "This a clothing App where users can navigate through their favourite dresses, add items to cart and place order. It is built using React and Sass",
-      link: "https://leo-clothing-app.netlify.app/",
-    },
-    {
-      image: sampleportfolio,
-      title: "Sample Portfolio",
-      description:
-        "This a an inspirational portfolio website designed by myself. It was built using Vanilla JavaScript and CSS",
-        link:"https://kortsa.github.io/project1/"
-    },
-    {
-      image: landingPage,
-      title: "Landing Page",
-      description:
-        "This a simple landing page i built when i started learning Javascript. i drew the inspiration from online. I used Html, Css and Javascript",
-        link:"https://kortsa.github.io/landingpage/"
-    },
-    {
-      image: votingapp,
-      title: "Voting App",
-      description:
-        "It is a voting app, that shows track of percentage of people that voted. It was built using html, css and javascript",
-        link:"https://kortsa.github.io/voting-system-js/"
-    },
-    {
-      image: quote,
-      title: "Quote Generator",
-      description:
-        "It is a quote generator app built using html, css and Javascript. The app displays random motivational quotes for the user when the user clicks on New quote",
-        link:"https://kortsa.github.io/quoteGenerator_js/"
-    },
-  ];
   return (
     <div className="project-content container">
       <div className="title">
